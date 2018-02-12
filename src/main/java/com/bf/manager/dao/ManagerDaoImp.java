e5e75de08d50c83b41aa7af35ed8543c22d7721d
김도현 <za4646@naver.com>
2018-02-12 16:06:56 +0900

    ver03_Dao에 SqlSessionTemplate변수 추가

--- src/main/java/com/bf/manager/dao/ManagerDaoImp.java
+++ src/main/java/com/bf/manager/dao/ManagerDaoImp.java
@@ -1,10 +1,17 @@
 package com.bf.manager.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 /**
  * @이름: 염현우
  * @날짜: 2018. 2. 12.
  * @설명: 
  */
+@Component
 public class ManagerDaoImp implements ManagerDao {
-
+	@Autowired
+	private SqlSessionTemplate sqlsession;
+	
 }
