665136df462b4cf60b2d75c892d400ea96a73b00
Sungho Park <nb211@naver.com>
2018-03-04 16:25:45 +0900

    작가 정보 수정 페이지 CSS 수정

--- src/main/webapp/resources/script/book/plus.js
+++ src/main/webapp/resources/script/book/plus.js
@@ -66,5 +66,5 @@ $(document).ready(function(){
 });
 
 function profilUpdate(root, author_num){
-	window.open(root+"/authorProfilUpdate.do?author_num="+author_num, 'update', 'top=100px, left=100px, height=300px, width=300px');
+	window.open(root+"/authorProfilUpdate.do?author_num="+author_num, 'update', 'top=100px, left=100px, height=450px, width=370px');
 }
